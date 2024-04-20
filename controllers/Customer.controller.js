4baa1e743f9b8469fbbd6a6bae24baa48cf8ffb5
yogesh6260 <yogeshtiwari6260@gmail.com>
2024-04-20 17:02:28 +0530

    routes added

--- controllers/Customer.controller.js
+++ controllers/Customer.controller.js
@@ -23,6 +23,22 @@ const createCustomer = async (req, res) => {
   }
 };
 
+// import/add customers
+const importCustomers = async (req, res) => {
+  try {
+    const docs = req.body;
+    const result = await Customers.insertMany(docs, { ordered: true });
+    console.log(`${result.length} Entries were inserted`);
+    res.status(201).json({
+      message: "Document Uploaded Successfully!",
+      result: result,
+    });
+  } catch (error) {
+    res.status(400).send("error", error);
+    console.log(error);
+  }
+};
+
 // edit customer details
 const updateCustomer = async (req, res) => {
   try {
@@ -54,21 +70,49 @@ const updateCustomer = async (req, res) => {
 
 // delete customer
 const deleteCustomer = async (req, res) => {
-  const { id } = req.params;
-  const customer = await Customers.findById(id); // Check if customer exists
-  if (!customer) {
-    res.status(404).send("Customer not found"); // Handle "not found" case
-    return;
+  try {
+    const { id } = req.params;
+    const customer = await Customers.findById(id); // Check if customer exists
+    if (!customer) {
+      res.status(404).send("Customer not found"); // Handle "not found" case
+      return;
+    }
+    await Customers.findByIdAndDelete(id);
+    res.status(200).json({
+      message: "Customer Deleted Successfully!",
+    });
+  } catch (error) {
+    console.error("Error deleting customer:", error); // Log detailed error
+    res.status(500).json({ error: "Internal server error" }); // Send generic error message
   }
-  await Customers.findByIdAndDelete(id);
-  res.status(200).json({
-    message: "Customer Deleted Successfully!",
-  });
+};
+
+// delete all customers
+const deleteAllCustomers = async (req, res) => {
+  const selectedCustomerIds = req.body;
+  try {
+    if (selectedCustomerIds && selectedCustomerIds.length > 0) {
+      await Customers.deleteMany({ _id: { $in: selectedCustomerIds } });
+      res.status(200).json({
+        message: "Selected customers deleted successfully!",
+      });
+    } else {
+      await Customers.deleteMany({});
+      res.status(200).json({
+        message: "All customers deleted successfully!",
+      });
+    }
+  } catch (error) {
+    console.error("Error deleting customers:", error); // Log detailed error
+    res.status(500).json({ message: "Error deleting customers!" }); // Send generic error message
+  }
 };
 
 module.exports = {
   getAllCustomers,
   createCustomer,
+  importCustomers,
   updateCustomer,
   deleteCustomer,
+  deleteAllCustomers,
 };
--- routes/Customer.route.js
+++ routes/Customer.route.js
@@ -2,8 +2,10 @@ const express = require("express");
 const {
   getAllCustomers,
   createCustomer,
+  importCustomers,
   updateCustomer,
   deleteCustomer,
+  deleteAllCustomers,
 } = require("../controllers/Customer.controller");
 
 const router = express.Router();
@@ -11,31 +13,45 @@ const router = express.Router();
 // routes
 
 // Method -> GET
-// Route -> /api/v1/customer/details/
+// Route -> /api/v1/customer/details
 // Controller -> getAllCustomers
 // Params -> None
 // Visibility -> Public
 router.get("/details", getAllCustomers);
 
 // Method -> POST
-// Route -> /api/v1/customer/new/
+// Route -> /api/v1/customer/new
 // Controller -> createCustomer
 // Params -> None
 // Visibility -> Public
 router.post("/new", createCustomer);
 
+// Method -> POST
+// Route -> /api/v1/customer/import
+// Controller -> importCustomers
+// Params -> None
+// Visibility -> Public
+router.post("/import", importCustomers);
+
 // Method -> PUT
 // Route -> /api/v1/customer/edit/:id
 // Controller -> updateCustomer
-// Params -> None
+// Params -> id
 // Visibility -> Public
 router.put("/edit/:id", updateCustomer);
 
 // Method -> DELETE
 // Route -> /api/v1/customer/delete/:id
 // Controller -> deleteCustomer
-// Params -> None
+// Params -> id
 // Visibility -> Public
 router.delete("/delete/:id", deleteCustomer);
+
+// Method -> DELETE
+// Route -> /api/v1/customer/delete/all
+// Controller -> deleteAllCustomers
+// Params -> None
+// Visibility -> Public
+router.delete("/deleteAll", deleteAllCustomers);
 
 module.exports = router;
